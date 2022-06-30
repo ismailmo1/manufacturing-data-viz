247e56640300b1d01acbd2210195d485e611b2d6
Ismail Mohammed <ismailmo4@gmail.com>
2022-06-30 22:03:02 +0100

    Cleanup comments

--- scheduling/script.js
+++ scheduling/script.js
@@ -379,20 +379,7 @@ function barClickHandler(event, elements) {
     orderDetailsCard.innerText = JSON.stringify(orderObj)
 
 }
-'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(201, 203, 207, 0.2)'
 
-'rgba(255, 99, 132, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(201, 203, 207, 0.2)'
 const comparisonChart = new Chart(comparisonCtx, {
     type: 'bar',
     labels: ['average', 'sum'],
@@ -441,3 +428,4 @@ function sumLateness(vfData) {
 
 }
 
+// TODO: add cleaning schedule optimisation
