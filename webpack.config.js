7d5451b7412417e81ffd36739dc94abd3bb2615a
Ismail Mohammed <ismailmo4@gmail.com>
2022-07-13 15:06:18 +0100

    split up files

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,63 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        port: 8000
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    port: 8000,
+  },
+  entry: {
+    home: "./src/script.js",
+    saltCurve: "./src/salt-curve/script.js",
+    scheduling: "./src/scheduling/scripts/main.js",
+    traceability: "./src/traceability/scripts/main.js",
+  },
+  output: {
+    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-    entry: {
-        home: './src/script.js',
-        saltCurve: './src/salt-curve/script.js',
-        scheduling: './src/scheduling/script.js',
-        traceability: './src/traceability/scripts/main.js',
-    },
-    output: {
-        filename: 'js/[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-        runtimeChunk: 'single',
-
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "index.html",
-            chunks: ['home']
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/scheduling/index.html",
-            filename: "scheduling/index.html",
-            chunks: ['scheduling']
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/salt-curve/index.html",
-            filename: "salt-curve/index.html",
-            chunks: ['saltCurve'],
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/scheduling/index.html",
-            filename: "scheduling/index.html",
-            chunks: ['scheduling']
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/traceability/index.html",
-            filename: "traceability/index.html",
-            chunks: ['traceability']
-        }),
-    ]
-
-}
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["home"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/scheduling/index.html",
+      filename: "scheduling/index.html",
+      chunks: ["scheduling"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/salt-curve/index.html",
+      filename: "salt-curve/index.html",
+      chunks: ["saltCurve"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/scheduling/index.html",
+      filename: "scheduling/index.html",
+      chunks: ["scheduling"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/traceability/index.html",
+      filename: "traceability/index.html",
+      chunks: ["traceability"],
+    }),
+  ],
+};
